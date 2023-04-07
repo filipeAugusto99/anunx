b877e47411fee16c464bee3696630489d691e820
Filipe Augusto <filipecordeiro213@gmail.com>
2023-04-07 17:44:00 -0300

    initial structure of project

---
+++ pages/_app.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Head from 'next/head'
+import { ThemeProvider } from '@mui/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import theme from '../src/theme'
+
+export default function MyApp(props) {
+  const { Component, pageProps } = props
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>My page</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width"></meta>
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </React.Fragment>
+  )
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  page: PropTypes.object.isRequired,
+}
